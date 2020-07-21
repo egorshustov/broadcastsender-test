b64feec562b9a30a3bc1b7bf54cb8e2e31a428d3
Egor Shustov <egorshustov.93@gmail.com>
2020-07-22 01:24:58 +0300

    Code refactored

--- app/src/main/java/com/egorshustov/broadcastsendertest/MainActivity.kt
+++ app/src/main/java/com/egorshustov/broadcastsendertest/MainActivity.kt
@@ -1,30 +1,32 @@
 package com.egorshustov.broadcastsendertest
 
-import android.content.*
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
-
 class MainActivity : AppCompatActivity() {
-    private lateinit var textView: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        textView = findViewById(R.id.text_view)
     }
 
     fun sendBroadcast(v: View) {
         val intent = Intent("com.egorshustov.EXAMPLE_ACTION")
+            .setPackage("com.egorshustov.broadcastreceivertest")
 
-        intent.setPackage("com.egorshustov.broadcastreceivertest")
+        val initialExtras = Bundle().apply {
+            putString(STRING_EXTRA_KEY, "Start")
+        }
 
-        val initialExtras = Bundle()
-        initialExtras.putString("stringExtra", "Start")
+        sendOrderedBroadcast(
+            intent, android.Manifest.permission.WAKE_LOCK,
+            SenderReceiver(), null, 0, "Start", initialExtras
+        )
+    }
 
-        sendOrderedBroadcast(intent, android.Manifest.permission.WAKE_LOCK,
-            SenderReceiver(), null, 0, "Start", initialExtras)
+    companion object {
+        const val STRING_EXTRA_KEY = "stringExtra"
     }
 }
--- app/src/main/java/com/egorshustov/broadcastsendertest/SenderReceiver.kt
+++ app/src/main/java/com/egorshustov/broadcastsendertest/SenderReceiver.kt
@@ -4,26 +4,28 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.widget.Toast
+import com.egorshustov.broadcastsendertest.MainActivity.Companion.STRING_EXTRA_KEY
 
 class SenderReceiver : BroadcastReceiver() {
+
     override fun onReceive(context: Context?, intent: Intent?) {
         var resultCode = resultCode
         var resultData = resultData
         val resultExtras = getResultExtras(true)
-        var stringExtra = resultExtras?.getString("stringExtra")
+        var stringExtra = resultExtras?.getString(STRING_EXTRA_KEY)
 
         ++resultCode
         stringExtra += "->SenderReceiver"
 
         val toastText = "SenderReceiver\n" +
-                "resultCode: " + resultCode + "\n" +
-                "resultData: " + resultData + "\n" +
-                "stringExtra: " + stringExtra
+                "resultCode: $resultCode\n" +
+                "resultData: $resultData\n" +
+                "stringExtra: $stringExtra"
 
         Toast.makeText(context, toastText, Toast.LENGTH_LONG).show()
 
         resultData = "SenderReceiver"
-        resultExtras.putString("stringExtra", stringExtra)
+        resultExtras.putString(STRING_EXTRA_KEY, stringExtra)
 
         setResult(resultCode, resultData, resultExtras)
     }
